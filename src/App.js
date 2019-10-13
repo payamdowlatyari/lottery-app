e6b5c984aab1e1586ad9cb8c02cd207c29940c7d
Payam Dowlatyari <pdowlatyari@gmail.com>
2019-10-12 17:23:40 -0700

    folder change

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Lottery from './Lottery';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Lottery/>
+      <Lottery title='Mini Daily' maxNum={10} maxBalls={4} />
+     
+    </div>
+  );
+}
+
+export default App;
